60305c278b1812d4de461df0d3459ff71b61e20b
ted.wen <ted.wen@ehsn.com.tw>
2021-10-29 17:14:34 +0800

    [publish: 1.0.0-beta.3]

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# inline-raw-source-plugin
+[![npm][npm]][npm-url]
+[![node][node]][node-url]
+
+<div align="center">
+  <h1>Inline Raw Source Plugin</h1>
+  <!-- <p></p> -->
+</div>
+
+<h2 align="center">Install</h2>
+
+```bash
+  npm i --save-dev inline-raw-source-plugin
+```
+
+```bash
+  yarn add --dev inline-raw-source-plugin
+```
+
+<h2 align="center">Usage</h2>
+
+The plugin will generate a file that you set the entries in your webpack config.
+The current state just allows to generate `html` or `cshtml` file with using `script` tags. 
+Just add the plugin to your `webpack`
+config as follows:
+
+**webpack.config.js**
+```js
+const InlineRawSourcePlugin = require('inline-raw-source-plugin')
+module.exports = {
+  entry: {
+      main: 'index.js',
+      inlineScript: 'inlineScript.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new InlineRawSourcePlugin({
+        chunkname: ["inlineScript"]
+    })
+  ]
+}
+```
+
+This will generate a file `dist/inline-source.html`(default) containing the following
+
+```html
+<script>{{your code}}</script>
+```
+
+⚠️ **The file you want to generate is only allowed to write as `ES5`(without `babel`).**
+
+<h2 align="center">Options</h2>
+
+Allowed values are as follows:
+
+|Name|Type|Default|Description|
+|:--:|:--:|:-----:|:----------|
+|**`chunkname`**|`{Array.<string>}`|`[]`|You have to specify the values as same as your entries when you need the entry chunk to generate a html file. |
+|**`filename`**|`{String}`|`'inline-source.html'`|The final file name for the generated HTML document|
+|**`path`**|`{string}`|`null`| If you set this option, you can change the output path that you set in webpack output config. |
+
+Here's an example webpack config illustrating how to use these options
+
+**webpack.config.js**
+```js
+{
+  entry: {
+      main: 'index.js',
+      inlineScript: 'inlineScript.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new InlineRawSourcePlugin({
+        chunkname: ["inlineScript"],
+        filename: "output.html",
+        path: path.resolve(__dirname, 'otherFolder'),
+    })
+  ]
+}
+```
--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-var { minify } = require("terser");
-var fs = require('fs');
-var path = require('path');
+const { minify } = require("terser");
+const fs = require('fs');
+const path = require('path');
 
 class InlineRawSourcePlugin {
     constructor(options) {
         this.options = options || {};
-        this.filename = this.options.filename || 'inline-script.html';
-        this.chunkname = this.options.chunkname || [];
+        this.filename = this.options.filename || 'inline-source.html';
+        this.chunkname = [...this.options.chunkname] || [];
         this.path = this.options.path || null;
+
+        this.notFoundChunkName = [...this.options.chunkname] || [];
         this.result = ""
+        this.hintMsg = "\"inline-raw-source-plugin\"\r\n";
     }
     apply(compiler) {
         // 源碼進行壓縮
         compiler.hooks.compilation.tap("inlineRawSourcePlugin", compilation => {
-            compilation.hooks.chunkAsset.tap("inlineRawSourcePlugin", chunk => {
-                if (this.chunkname.indexOf(chunk.name) > -1) {
-                    let result = minify(chunk.entryModule._source._value).code
+            compilation.hooks.chunkAsset.tap("inlineRawSourcePlugin", async chunk => {
+                const chunkIndex = this.chunkname.indexOf(chunk.name);
+                if (chunkIndex > -1) {
+                    let result = await minify(chunk.entryModule._source._value).code
                     this.result += `<script>${result}</script>`
+                    this.notFoundChunkName.splice(chunkIndex, 1)
                 }
             });
+        });
+
+        // 印出找不到的 chunkname
+        compiler.hooks.afterCompile.tapAsync('inlineRawSourcePlugin', (compilation, cb) => {
+            this.notFoundChunkName.length && compilation.warnings.push(`${this.hintMsg}找不到指定的 chunkname: [${this.notFoundChunkName.join(", ")}]`);
+
+            cb();
         });
 
         // 產生我們要的 html 檔
         compiler.hooks.emit.tapAsync('inlineRawSourcePlugin', (compilation, cb) => {
             const fileListName = this.filename;
             const result = this.result;
-            compilation.assets[fileListName] = {
-                source: function () {
-                    return result;
-                },
-                size: function () {
-                    return result.length;
+            if (result.length) {
+                compilation.assets[fileListName] = {
+                    source: function () {
+                        return result;
+                    },
+                    size: function () {
+                        return result.length;
+                    }
                 }
+            } else {
+                compilation.warnings.push(`${this.hintMsg}並未產生任何 inline-source 代碼，因此無法產出 ${fileListName}`);
             }
-            
             cb();
         });
 
@@ -43,19 +58,24 @@ class InlineRawSourcePlugin {
                 return;
             }
             const assets = stats.toJson().assets.map(asset => asset.name);
-            const matchFiles = this.chunkname.map(chunkname => {
+
+            const matchFiles = this.chunkname.reduce((result, chunkname) => {
                 const filename = assets.find(asset => asset.includes(chunkname))
                 const regExp = /.*\.js/;
-                return filename.match(regExp)[0]
-            })
+                filename && regExp.test(filename) && result.push(filename.match(regExp)[0])
+                return result
+            }, [])
+
+            if (!matchFiles.length) return;
+            
             matchFiles.forEach(matchFile => {
-                fs.unlinkSync(path.join(path.dirname(module.parent.filename), `${stats.compilation.outputOptions.publicPath}/${matchFile}`))
+                fs.unlinkSync(path.join(stats.compilation.outputOptions.path, matchFile))
             });
 
             if (this.path) {
                 fs.rename(
-                    path.join(path.dirname(module.parent.filename), `${stats.compilation.outputOptions.publicPath}/${this.filename}`), 
-                    `${this.path}/${this.filename}`, function (err) {
+                    path.join(stats.compilation.outputOptions.path, this.filename), 
+                    path.join(this.path, this.filename), err => {
                         if (err) {
                             throw err;
                         }
