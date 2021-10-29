eb67e539419ff736276e43cf874bdd60d56d9689
ted.wen <ted.wen@ehsn.com.tw>
2021-10-29 10:46:06 +0800

    [1.0.0-beta.2]

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ var { minify } = require("terser");
 var fs = require('fs');
 var path = require('path');
 
-class InlineScriptPlugin {
+class InlineRawSourcePlugin {
     constructor(options) {
         this.options = options || {};
         this.filename = this.options.filename || 'inline-script.html';
+        this.chunkname = this.options.chunkname || [];
+        this.path = this.options.path || null;
         this.result = ""
     }
     apply(compiler) {
         // 源碼進行壓縮
-        compiler.hooks.compilation.tap("inlineScriptPlugin", compilation => {
-            compilation.hooks.chunkAsset.tap("inlineScriptPlugin", chunk => {
-                let result = minify(chunk.entryModule._source._value).code
-                this.result += `<script>${result}</script>`
+        compiler.hooks.compilation.tap("inlineRawSourcePlugin", compilation => {
+            compilation.hooks.chunkAsset.tap("inlineRawSourcePlugin", chunk => {
+                if (this.chunkname.indexOf(chunk.name) > -1) {
+                    let result = minify(chunk.entryModule._source._value).code
+                    this.result += `<script>${result}</script>`
+                }
             });
         });
 
         // 產生我們要的 html 檔
-        compiler.hooks.emit.tapAsync('inlineScriptPlugin', (compilation, cb) => {
+        compiler.hooks.emit.tapAsync('inlineRawSourcePlugin', (compilation, cb) => {
             const fileListName = this.filename;
             const result = this.result;
             compilation.assets[fileListName] = {
@@ -34,20 +38,31 @@ class InlineScriptPlugin {
         });
 
         // 刪除不必要的 output 檔案
-        compiler.hooks.done.tap('inlineScriptPlugin', stats => {
+        compiler.hooks.done.tap('inlineRawSourcePlugin', stats => {
             if (compiler.outputFileSystem.constructor.name !== "NodeOutputFileSystem") {
                 return;
             }
             const assets = stats.toJson().assets.map(asset => asset.name);
-            const matchFiles = assets.reduce((result, asset) => {
+            const matchFiles = this.chunkname.map(chunkname => {
+                const filename = assets.find(asset => asset.includes(chunkname))
                 const regExp = /.*\.js/;
-                if (regExp.test(asset)) result.push(asset.match(regExp)[0])
-                return result
-            }, []);
+                return filename.match(regExp)[0]
+            })
             matchFiles.forEach(matchFile => {
-                fs.unlinkSync(path.join(path.dirname(module.parent.filename), `/dist/${matchFile}`))
+                fs.unlinkSync(path.join(path.dirname(module.parent.filename), `${stats.compilation.outputOptions.publicPath}/${matchFile}`))
             });
+
+            if (this.path) {
+                fs.rename(
+                    path.join(path.dirname(module.parent.filename), `${stats.compilation.outputOptions.publicPath}/${this.filename}`), 
+                    `${this.path}/${this.filename}`, function (err) {
+                        if (err) {
+                            throw err;
+                        }
+                    }
+                )
+            }
         })
     }
 }
-module.exports = InlineScriptPlugin;
+module.exports = InlineRawSourcePlugin;
